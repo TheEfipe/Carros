6ddf4afbf82b45aac92ed3fa67f2f109edfeb3ab
TheEfipe <79647496+TheEfipe@users.noreply.github.com>
2021-03-02 10:42:35 -0300

    Create ClasseMain.java

---
+++ ClasseMain.java
@@ -0,0 +1,10 @@
+public class ClasseMain {
+
+
+    public static void main(String... args) {
+        Carro rodas = new Carro(4, 2020, 4854894, "Gasolina"); // Quantidade de rodas / ano do carro / Chassi do carro (7 digitos) / Tipo de Combustivel
+        rodas.imprimeValores(); // Imprimir dados do carro
+
+    }
+
+}
